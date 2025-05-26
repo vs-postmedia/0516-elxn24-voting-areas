03cf187e9fd4e229f5cb68909d17c5b5bf2f28f3
Nathan Griffiths <ngriffiths@postmedia.com>
2025-05-26 14:29:33 -0700

    copy edits

--- src/data/steps.js
+++ src/data/steps.js
@@ -1,6 +1,5 @@
 export default [
-    'Detailed results from B.C.’s 2024 provincial election show a significant geographic consolidation of NDP and Conservative voters – particularly in Metro Vancouver – compared to 2020, according to a Postmedia analysis.
- <span class="scroll-more">Scroll for more...</span>',
+    'Detailed results from B.C.’s 2024 provincial election show a significant geographic consolidation of NDP and Conservative voters – particularly in Metro Vancouver – compared to 2020, according to a Postmedia analysis <span class="scroll-more">Scroll for more...</span>',
     'Support for the <span style="color:#ff6633;font-family:BentonSansCond-Bold;">B.C. NDP</span> solidified in Vancouver, New Westminster, the Tri-cities and much of North Vancouver.',
     '<span style="color:#133968;font-family:BentonSansCond-Bold;">Conservative Party</span> support coalesced in Richmond...',
     '...parts of Surrey...',
